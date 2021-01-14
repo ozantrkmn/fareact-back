a9340291991dceae8453d297a90c48334bdbb700
Ozan Türkmen <24991644+ozantrkmn@users.noreply.github.com>
2021-01-14 19:21:18 +0300

    Initial commit

---
+++ src/main/java/org/ozan/fareactback/fareactBackApplication.java
@@ -0,0 +1,18 @@
+package org.ozan.fareactback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Libprov2Application {
+
+	public static void main(String[] args) {
+	    
+		
+		SpringApplication.run(Libprov2Application.class, args);
+
+
+
+	}
+
+}
